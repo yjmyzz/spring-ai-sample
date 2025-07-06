58b18df8d2a8843f2327fa882812c038cf9e1933
junmingyang <junmingyang@trip.com>
2025-07-06 17:43:45 +0800

    mcp test

--- src/main/java/com/cnblogs/yjmyzz/mcp/server/AuthorService.java
+++ src/main/java/com/cnblogs/yjmyzz/mcp/server/AuthorService.java
@@ -14,6 +14,6 @@ public class AuthorService {
     @Tool(name = "getCnBlogsUrlByName",
             description = "获取cnblogs某博主的博客网址")
     public String getCnBlogsUrlByName(@ToolParam(required = true, description = ",比如：菩提树下的杨过") String bloggerName) {
-        return "博主(" + bloggerName + ")的博客地址是：https://www.cnblogs.com/yjmyzz";
+        return "https://www.cnblogs.com/yjmyzz";
     }
 }
